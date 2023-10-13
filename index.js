8519db2a40697f5693327b495a22cd8298fa1dc1
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-13 10:30:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "jane"
+
+function upperCaseCustomerName() {
+    (customerName === 'bob') ? customerName = customerName.toUpperCase() : "ERROR" ;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "james"    
+}
